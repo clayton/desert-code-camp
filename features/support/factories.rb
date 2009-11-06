17aceb5a51d4e350aefa76d5326c5a17f28c65cf
Clayton Lengel-Zigich <clayton@claytonlz.local>
2009-11-05 22:42:18 -0700

    Add some styling, fix some authlogic errors, remove index.html

--- features/support/factories.rb
+++ features/support/factories.rb
@@ -1,1 +1,12 @@
-# Factory Girl Factories
+# Factory Girl Factories and Sequences
+
+Factory.sequence(:email) do |number|
+  "user-#{number}@example.org"
+end
+
+Factory.define :user do |user|
+  user.email Factory.next(:email)
+  user.password "password"
+  user.password_confirmation "password"
+  user.single_access_token "k3cFzLIQnZ4MHRmJvJzg"
+end
